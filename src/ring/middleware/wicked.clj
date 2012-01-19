aca6e9215087e9e22a15a751cd4581df9fce5dca
Greg Berenfield <gberenfield@berenfield.com>
2012-01-19 17:56:17 -0500

    first stab at basic functionality

--- src/ring/middleware/wicked.clj
+++ src/ring/middleware/wicked.clj
@@ -1,1 +1,24 @@
-(ns ring-wicked-pdf.core)
+(ns ring.middleware.wicked
+  (:use [clojure.java.shell :only [sh]])
+  (:import (java.io InputStream
+                    File
+                    ByteArrayInputStream
+                    ByteArrayOutputStream)))
+
+
+(defn wrap-wicked-pdf [handler]
+  (fn [req]
+    (let [{body :body
+           status :status
+           :as resp} (handler req)]
+      (if (and (= status 200)
+               (not (get-in resp [:headers "content-encoding"])) ; no content-encoding defined
+               (or
+                 (and (string? body) (> (count body) 200))
+                 (instance? InputStream body)
+                 (instance? File body)))
+        (let [tehpdf (:err (sh "usr/local/bin/wkhtmltopdf" {:in body}))]
+        ; should have an if stmt check return code from wkhtmltopdf and
+        ; render resp if error
+          (merge resp {:body tehpdf :content-type "application/x-pdf"}))
+        resp))))
