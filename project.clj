7c4bfab5197d83935d23d315dcdd903866440d95
Greg Berenfield <gberenfield@berenfield.com>
2012-11-13 10:02:49 -0500

    bump project to 0.2.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject ring-wicked-pdf "0.1.0-SNAPSHOT"
+(defproject ring-wicked-pdf "0.2.0"
   :description "wkhtmltopdf pdf generating middleware"
   :dependencies [[org.clojure/clojure "1.3.0"]])
