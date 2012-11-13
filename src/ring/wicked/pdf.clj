7a67df7577cacceb8408b675429744cfba9e7e47
Greg Berenfield <gberenfield@berenfield.com>
2012-11-13 10:03:22 -0500

    fixed resources not being parsed (css/images etc)

--- src/ring/wicked/pdf.clj
+++ src/ring/wicked/pdf.clj
@@ -2,8 +2,20 @@
   (:require [clojure.java.io :as io])
   (:use [clojure.java.shell :only [sh]]))
 
-(defn as-pdf [contents]
-  (let [tempfile (io/file (str "resources/public/tmp/foo.pdf"))
-        tempfilename (str (. tempfile getAbsoluteFile))
-        tehpdf (:out (sh "/usr/local/bin/wkhtmltopdf" "-O" "landscape" "-" tempfilename :in contents))]
-    tempfile))
+(defn as-pdf
+  "Takes an entire html page as contents and defaults to
+  'resources/public/ for any css/js/img items in the path
+  of the running clojure web app. Optionally, a 'resources'
+  subdir can be passed as well."
+  ([contents]
+  (let [fixed-content (clojure.string/replace page #"\"/" (str "\"" (System/getProperty "user.dir") "/resources/public/"))
+        temp-file (io/file (str "resources/public/tmp/foo.pdf"))
+        temp-filename (str (. temp-file getAbsoluteFile))
+        pdf (:out (sh "/usr/local/bin/wkhtmltopdf" "-O" "landscape" "-" temp-filename :in fixed-content))]
+    temp-file))
+  ([contents resources]
+  (let [fixed-content (clojure.string/replace page #"\"/" (str "\"" (System/getProperty "user.dir") resources))
+        temp-file (io/file (str "resources/public/tmp/foo.pdf"))
+        temp-filename (str (. temp-file getAbsoluteFile))
+        pdf (:out (sh "/usr/local/bin/wkhtmltopdf" "-O" "landscape" "-" temp-filename :in fixed-content))]
+    temp-file)))
